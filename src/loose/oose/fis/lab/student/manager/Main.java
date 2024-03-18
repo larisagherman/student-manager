4a11bd2f0dfff4323edceaef5a9cb5026b8a8fdd
David NAN <dnan@bitdefender.com>
2024-03-18 15:11:00 +0200

    SM-2 Added the students Table and some dummy student list

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -4,4 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+        primaryStage.setTitle("Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
 }
